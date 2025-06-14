81e5c8a66dfdedf82cbbfd7d811378a02e975697
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-14 20:09:09 -0300

    feat: implement HomeViewModel with VNI validation logic and state management

--- lib/home/view_model/home_view_model.dart
+++ lib/home/view_model/home_view_model.dart
@@ -1,3 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomeViewModel extends ChangeNotifier {}
+import 'vni_use_case.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  HomeViewModel({
+    required VNIValidationUseCase vniValidationUseCase,
+  }) : _vniValidationUseCase = vniValidationUseCase;
+
+  final VNIValidationUseCase _vniValidationUseCase;
+
+  String _vni = '';
+  String? _vniError;
+
+  String get vni => _vni;
+  String? get vniError => _vniError;
+
+  void onVniChanged(String value) {
+    _vni = value;
+    _vniError = _vniValidationUseCase(_vni);
+    notifyListeners();
+  }
+
+  void clearVni() {
+    _vni = '';
+    _vniError = null;
+    notifyListeners();
+  }
+}
