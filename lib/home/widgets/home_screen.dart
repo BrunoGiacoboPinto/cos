155924ccb043c7d7934cbc92a8931f8e90359efa
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-14 11:05:00 -0300

    feat: update HomeScreen to require HomeViewModel and enhance VIN input field

--- lib/home/widgets/home_screen.dart
+++ lib/home/widgets/home_screen.dart
@@ -1,7 +1,13 @@
+import 'package:cos/home/view_model/home_view_model.dart';
 import 'package:flutter/material.dart';
 
 final class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({
+    super.key,
+    required this.viewModel,
+  });
+
+  final HomeViewModel viewModel;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +20,7 @@ final class HomeScreen extends StatelessWidget {
             decoration: InputDecoration(
               hintText: 'Enter the VIN of the vehicle you are looking for',
               border: const OutlineInputBorder(),
+              suffixIcon: Icon(Icons.search),
             ),
             keyboardType: TextInputType.text,
             maxLength: 17,
