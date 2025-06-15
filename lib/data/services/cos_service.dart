f8a3744b3964ff2565fd9cfdb25a912354d5ab45
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-14 22:32:28 -0300

    feat: enhance CosService implementation with headers and improved response handling

--- lib/data/services/cos_service.dart
+++ lib/data/services/cos_service.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:cos/config/environment.dart';
+import 'package:cos/data/api/cos_api.dart';
 import 'package:cos/data/model/cos_response.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
 abstract interface class CosService {
@@ -15,12 +17,25 @@ final class CosServiceImpl implements CosService {
 
   @override
   Future<CosResponse> getData() async {
-    final response = await _httpClient.get(Environment.baseUrl);
+    final response = await _httpClient.get(
+      Environment.baseUrl,
+      headers: {
+        CosChallenge.user: 'user',
+      },
+    );
+
+    debugPrint('__response = ${response.body}');
 
     return switch (response.statusCode) {
-      200 => CosResponseWithData.fromJson(jsonDecode(response.body) as Map<String, Object?>),
-      300 => CosResponseWithChoices.fromJson(jsonDecode(response.body) as Map<String, Object?>),
-      _ => CosResponseWithError.fromJson(jsonDecode(response.body) as Map<String, Object?>),
+      200 => CosResponse.data(
+        data: CosResponseData.fromJson(jsonDecode(response.body) as Map<String, Object?>),
+      ),
+      300 => CosResponse.choices(
+        choices: [...(jsonDecode(response.body) as List<Object?>).cast<Map<String, Object?>>().map(CosResponseChoiches.fromJson)],
+      ),
+      _ => CosResponse.error(
+        error: CosResponseError.fromJson(jsonDecode(response.body) as Map<String, Object?>),
+      ),
     };
   }
 }
