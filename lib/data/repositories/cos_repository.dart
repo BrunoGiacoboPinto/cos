70e3a70730ee1ec3a3835fcb0fc2f40ca4cd06c2
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-17 18:23:56 -0300

    refactor: remove debug print statements from CosRepository for cleaner code

--- lib/data/repositories/cos_repository.dart
+++ lib/data/repositories/cos_repository.dart
@@ -1,6 +1,5 @@
 import 'package:cos/data/cos_data_access_interface.dart';
 import 'package:cos/domain/model/car_auction.dart';
-import 'package:flutter/rendering.dart';
 
 class CosRepository {
   CosRepository({
@@ -23,7 +22,6 @@ class CosRepository {
 
     final localData = await localService.getAllData();
     if (localData.isNotEmpty) {
-      debugPrint('__Fetching from localService, saving to memory cache for ${localData.length} entries');
       await Future.wait(
         localData.entries.map((entry) async {
           await memoryCosService.saveData(data: entry.value, key: entry.key);
@@ -38,10 +36,8 @@ class CosRepository {
 
   Future<CarAuctionModel?> getCarAuction({required String key}) async {
     if (await memoryCosService.hasData(key: key)) {
-      debugPrint('__Fetching from memory cache for key: $key');
       return await memoryCosService.getData(key: key);
     } else if (await localService.hasData(key: key)) {
-      debugPrint('__Fetching from localService for key: $key');
       final data = await localService.getData(key: key);
       if (data != null) {
         await memoryCosService.saveData(data: data, key: key);
@@ -52,10 +48,7 @@ class CosRepository {
 
       final data = CarAuctionModel.fromResponse(response);
 
-      debugPrint('__Fetching from Remote for key: $key for (${data.runtimeType})');
-
       if (data is CarAuctionWithData) {
-        debugPrint('__Saving from remote for key: $key');
         await localService.saveData(data: data, key: key);
         await memoryCosService.saveData(data: data, key: key);
       }
