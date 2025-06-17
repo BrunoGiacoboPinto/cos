661e7c3a6d1d1a97089144b3f3c0cdcd72207d44
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-16 22:13:38 -0300

    feat: enhance routing with PageViewTransitionPage for smoother navigation and custom splash effect

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,4 +1,6 @@
 import 'package:cos/home/widgets/home_screen.dart';
+import 'package:cos/routing/transitions.dart';
+import 'package:cos/ui/core/ui/theme/button.dart';
 import 'package:cos/ui/core/ui/theme/colors.dart';
 import 'package:cos/vehicle_auction/widgets/vehicle_auction_screen.dart';
 import 'package:flutter/material.dart';
@@ -22,42 +24,52 @@ GoRouter buildAppRouter(GetIt dependencies) {
           return Scaffold(
             backgroundColor: darkGray,
             body: SafeArea(child: child),
-            bottomNavigationBar: BottomNavigationBar(
-              backgroundColor: darkGray,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.gavel),
-                  label: 'Auction',
-                ),
-              ],
-              currentIndex: AppRoutes.values.indexWhere(
-                (route) => route.path == state.uri.path,
+            bottomNavigationBar: Theme(
+              data: Theme.of(context).copyWith(
+                splashFactory: const LimitedRadiusSplashFactory(),
               ),
-              onTap: (index) {
-                if (index == 0) {
-                  context.go(AppRoutes.home.path);
-                } else {
-                  context.go(AppRoutes.auction.path);
-                }
-              },
+              child: BottomNavigationBar(
+                elevation: 0,
+                backgroundColor: darkGray,
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.gavel),
+                    label: 'Auction',
+                  ),
+                ],
+                currentIndex: AppRoutes.values.indexWhere(
+                  (route) => route.path == state.uri.path,
+                ),
+                onTap: (index) {
+                  context.go(AppRoutes.values[index % AppRoutes.values.length].path);
+                },
+              ),
             ),
           );
         },
         routes: [
           GoRoute(
             path: AppRoutes.home.path,
-            builder: (context, state) {
-              return HomeScreen(viewModel: dependencies.get());
+            pageBuilder: (context, state) {
+              return PageViewTransitionPage(
+                child: HomeScreen(viewModel: dependencies.get()),
+                pageKey: state.pageKey,
+                pageIndex: AppRoutes.home.index,
+              );
             },
           ),
           GoRoute(
             path: AppRoutes.auction.path,
-            builder: (context, state) {
-              return VehicleAuctionScreen(viewModel: dependencies.get());
+            pageBuilder: (context, state) {
+              return PageViewTransitionPage(
+                child: VehicleAuctionScreen(viewModel: dependencies.get()),
+                pageKey: state.pageKey,
+                pageIndex: AppRoutes.auction.index,
+              );
             },
           ),
         ],
