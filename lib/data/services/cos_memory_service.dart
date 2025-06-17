9d7b65221beaf681ad20ce1a5cfa9ea936f144e2
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-16 23:41:07 -0300

    feat: implement CosMemoryService for in-memory data access of CarAuctionModel

---
+++ lib/data/services/cos_memory_service.dart
@@ -0,0 +1,25 @@
+import 'package:cos/data/cos_data_access_interface.dart';
+import 'package:cos/domain/model/car_auction.dart';
+
+final class CosMemoryService implements CosServiceReadWriteAccess<CarAuctionModel> {
+  final Map<String, CarAuctionModel> _cache = {};
+
+  @override
+  Future<CarAuctionModel?> getData({String? key}) async {
+    if (key == null) {
+      return null;
+    } else {
+      return _cache[key];
+    }
+  }
+
+  @override
+  Future<bool> hasData({required String key}) async {
+    return _cache.containsKey(key);
+  }
+
+  @override
+  Future<void> saveData({required CarAuctionModel data, required String key}) async {
+    _cache[key] = data;
+  }
+}
