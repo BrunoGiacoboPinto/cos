6c6e209784b4cea48d9f8f84c114f539c9952f8f
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-18 17:07:55 -0300

    feat: update VehicleAuctionScreen to display auction data with improved layout and state handling

--- lib/vehicle_auction/widgets/vehicle_auction_screen.dart
+++ lib/vehicle_auction/widgets/vehicle_auction_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cos/ui/core/ui/theme/colors.dart';
+import 'package:cos/ui/core/ui/theme/spacing.dart';
+import 'package:cos/ui/core/ui/vehicle_auction_list.dart';
 import 'package:cos/vehicle_auction/view_model/vehicle_auction_view_model.dart';
 import 'package:flutter/material.dart';
 
@@ -11,15 +14,41 @@ final class VehicleAuctionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text('Welcome to the Vehicle Auction!'),
-          SizedBox(height: 20),
-          Text('Here you can bid on various vehicles.'),
-        ],
-      ),
+    return Column(
+      children: [
+        spacing2Xl,
+        Center(
+          child: Text(
+            'Last vehicles auctioned',
+            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+              fontWeight: FontWeight.bold,
+              color: lightBlue,
+            ),
+          ),
+        ),
+        spacingSm,
+        Expanded(
+          child: ListenableBuilder(
+            listenable: viewModel,
+            builder: (context, child) {
+              return DefaultTextStyle(
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: lightBlue,
+                ),
+                child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  child: switch (viewModel.state) {
+                    VehicleAuctionScreenStateInitial() => const Center(child: Text('No Vehicle Auction has been made yet')),
+                    VehicleAuctionScreenStateLoading() => const Center(child: CircularProgressIndicator()),
+                    VehicleAuctionScreenStateLoaded(data: final data) => VehicleAuctionList(models: data),
+                    VehicleAuctionScreenStateError(error: final error) => Center(child: Text('Error: $error')),
+                  },
+                ),
+              );
+            }, 
+          ),
+        ),
+      ],
     );
   }
 }
