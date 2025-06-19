3f84e5500ec018862b3614923f2ded8154674814
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-18 22:39:42 -0300

    feat: update AppBarTheme for improved styling and consistency

--- lib/ui/core/ui/theme/app_bar.dart
+++ lib/ui/core/ui/theme/app_bar.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'colors.dart';
 
 final appBarTheme = AppBarTheme(
-  backgroundColor: pureWhite,
-  foregroundColor: darkGray,
+  backgroundColor: darkGray,
   elevation: 0,
-  shadowColor: Colors.transparent,
-  surfaceTintColor: Colors.transparent,
   titleTextStyle: TextStyle(
-    fontSize: 20.0,
-    fontWeight: FontWeight.w600,
-    color: darkGray,
+    fontWeight: FontWeight.bold,
+    color: lightBlue,
   ),
-  toolbarHeight: 72.0,
-  shape: Border(
-    bottom: BorderSide(
-      color: borderLight,
-      width: 1.0,
-    ),
+  centerTitle: true,
+  iconTheme: IconThemeData(color: lightBlue),
+  systemOverlayStyle: SystemUiOverlayStyle.light.copyWith(
+    statusBarColor: darkGray,
+    statusBarIconBrightness: Brightness.light,
   ),
 );
