7a15e9b053ff8653335b9238ed042515a8c69b6e
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-19 17:51:35 -0300

    feat: enhance CosStorageService to manage allowed keys and improve data retrieval

--- lib/data/services/cos/cos_storage_service.dart
+++ lib/data/services/cos/cos_storage_service.dart
@@ -3,10 +3,28 @@ import 'dart:convert';
 
 import 'package:cos/data/services/cos/cos_data_access_interface.dart';
 import 'package:cos/domain/model/car_auction.dart';
+import 'package:logging/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final class CosStorageService implements CosServiceReadWriteAccess<CarAuctionModel> {
+  CosStorageService() {
+    _allowedKeysCompleter = Completer<void>();
+    _allowedKeysCompleter.complete(
+      _sharedPreferences.getStringList(_kCarAuctionStorageKeys).then(
+        (keys) {
+          _allowedKeys.addAll(keys ?? []);
+          _logger.info('Allowed keys initial load: $_allowedKeys');
+        },
+      ),
+    );
+  }
+
+  static const String _kCarAuctionStorageKeys = 'car_auction_storage_keys';
+  static final _logger = Logger('CosStorageService');
+
   late final _sharedPreferences = SharedPreferencesAsync();
+  late final Set<String> _allowedKeys = {};
+  late final Completer<void> _allowedKeysCompleter;
 
   @override
   Future<CarAuctionModel?> getData({required String key}) async {
@@ -32,12 +50,24 @@ final class CosStorageService implements CosServiceReadWriteAccess<CarAuctionMod
   Future<void> saveData({required CarAuctionModel data, required String key}) async {
     if (data is CarAuctionWithData) {
       await _sharedPreferences.setString(key, jsonEncode(data.toJson()));
+      _sharedPreferences.getStringList(_kCarAuctionStorageKeys).then(
+        (keys) {
+          _allowedKeys.addAll([...?keys, key]);
+          _logger.info('Got $key and Will save: $_allowedKeys');
+
+          _sharedPreferences.setStringList(
+            _kCarAuctionStorageKeys,
+            _allowedKeys.toList(),
+          );
+        },
+      );
     }
   }
 
   @override
   Future<Map<String, CarAuctionModel>> getAllData() async {
-    final keys = await _sharedPreferences.getKeys();
+    await _allowedKeysCompleter.future;
+    final keys = await _sharedPreferences.getKeys(allowList: _allowedKeys);
 
     return <String, CarAuctionModel>{
       for (final key in keys)
