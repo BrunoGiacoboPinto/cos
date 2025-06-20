e28197fcb44cdd264d55ae1016cb8a2b6c4ca328
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-20 11:12:28 -0300

    refactor: update LoginService to use final class and improve shared preferences handling

--- lib/data/services/login/login_service.dart
+++ lib/data/services/login/login_service.dart
@@ -1,9 +1,10 @@
-
+import 'package:meta/meta.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginService {
+final class LoginService {
+  LoginService({@visibleForTesting SharedPreferencesAsync? sharedPreferences}) : _sharedPreferences = sharedPreferences ?? SharedPreferencesAsync();
+  late final SharedPreferencesAsync _sharedPreferences;
   static const _kUserLoginKey = 'user_login';
-  late final _sharedPreferences = SharedPreferencesAsync();
 
   Future<void> saveUserLogin(String email) async {
     await _sharedPreferences.setString(_kUserLoginKey, email);
