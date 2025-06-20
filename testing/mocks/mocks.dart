a3a21c4ed31f506cca39b92d9f7802eb04b77f30
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-06-20 11:51:27 -0300

    feat: enhance testing capabilities by adding mock services for CosServiceAccess and CosServiceReadWriteAccess

--- testing/mocks/mocks.dart
+++ testing/mocks/mocks.dart
@@ -1,4 +1,15 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cos/data/services/cos/cos_data_access_interface.dart';
+import 'package:cos/domain/model/car_auction.dart';
+import 'package:cos/data/model/cos_response.dart';
 
 final class SharedPreferencesAsyncMock extends Mock implements SharedPreferencesAsync {}
+
+// Local abstract classes to bypass interface class restrictions for repository tests
+abstract class TestCosServiceAccess implements CosServiceAccess<CosResponse> {}
+abstract class TestCosServiceReadWriteAccess implements CosServiceReadWriteAccess<CarAuctionModel> {}
+
+class MockRemoteService extends Mock implements TestCosServiceAccess {}
+class MockLocalService extends Mock implements TestCosServiceReadWriteAccess {}
+class MockMemoryService extends Mock implements TestCosServiceReadWriteAccess {}
